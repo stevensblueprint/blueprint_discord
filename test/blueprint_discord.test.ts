6b28afb3d05cc2abc8856c686e01579b60c2d3d9
miguel-merlin <mmerlin@stevens.edu>
2026-02-11 00:49:11 -0500

    Initial commit

---
+++ test/blueprint_discord.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlueprintDiscord from '../lib/blueprint_discord-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blueprint_discord-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlueprintDiscord.BlueprintDiscordStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
